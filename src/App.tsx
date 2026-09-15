2970720ad0bfeb97dd5ab328d269dd818b5e6e84
kenshien <kenshienworkacc@gmail.com>
2026-09-15 09:25:39 +0800

    feat: add project preview layout and also the stye

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import Header from "./components/Header";
+
 function App() {
-  return <div>Hi from Nova Tech</div>;
+  return (
+    <>
+      <Header />
+      <main id="main">
+        {/* <Hero />
+        <Categories />
+        <FeaturedProducts />
+        <StoreBenefits />
+        <Promotion />
+        <ProductShowcase />
+        <Newsletter />
+        <Testimonials />
+        <StoreInfo />
+        <FAQ />
+        <FinalCTA /> */}
+      </main>
+      {/* <Footer /> */}
+    </>
+  );
 }
 
 export default App;
